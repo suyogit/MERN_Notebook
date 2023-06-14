de3fc79f05f12f60b2aba186d674832e6daf4f7b
Suyog Acharya <asuyog042@gmail.com>
2023-06-14 21:31:21 +0545

    database and express server connected

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+// //mongoDB validation,casting and business logic is a lenthy process
+// //so use mongoose
+
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB successfully");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+module.exports = connectToMongo;
