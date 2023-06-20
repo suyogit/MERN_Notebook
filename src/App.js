b3797ce7ee3ab7bc2604aa123173c4e02a7f1478
Suyog Acharya <asuyog042@gmail.com>
2023-06-20 20:55:32 +0545

    useContext hook implemented

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ import {
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 function App() {
   return (
     <>
+      <NoteState>
       <Router>
         <Navbar />
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
+            <Route exact path="/about" element={<About />} />
         </Routes>
       </Router>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import { React, useContext } from 'react'
+import { useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
 const About = () => {
+
+
+    const a = useContext(noteContext)
+
+    useEffect(() => {
+        a.update()
+        // eslint-disable-next-line
+    }, [])
+
     return (
-        <div>This is about</div>
+        <div>This is about {a.state.name} and he studies in {a.state.class}</div>
     )
 }
 
